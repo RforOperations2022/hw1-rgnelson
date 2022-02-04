dda6674c33233a6698c7760c36f9a182cc81e48f
Bobby Nelson <rnelsonxy@gmail.com>
2022-02-04 12:50:42 -0500

    Adds a tabular view and download button
    Adds a tabular view with corresponding download button for the topic/state level data.

--- app/app.R
+++ app/app.R
@@ -4,6 +4,7 @@ library(forcats)
 library(shiny)
 library(ggplot2)
 library(usmap)
+library(DT)
 
 # read the data
 data <- readRDS(file="cdi_data.rds")
@@ -46,7 +47,11 @@ ui <- fluidPage(
             
             plotOutput("us.map"),
             
-            plotOutput("us.barplot")
+            tabsetPanel(
+                tabPanel("Plot", plotOutput("us.barplot")),
+                tabPanel("Table", DT::dataTableOutput("us.table"),
+                                  downloadButton("download.button"))
+            )
         )
     )
 )
@@ -60,11 +65,21 @@ server <- function(input, output) {
         req(input$state)
         data %>% 
             filter(Topic == input$cdi,
-                   YearStart == paste0(input$year)) %>%
+                   YearStart == paste0(input$year),
+                   Stratification1 == "Overall") %>%
             group_by(Topic, state, Stratification1) %>%
             summarize(total = sum(DataValue, na.rm=TRUE)) %>%
             mutate(selected.state = ifelse(state == input$state,
                                            TRUE, FALSE))
+    })
+    
+    # pretty data table output
+    data.topic.subset.pretty <- reactive({
+        data.topic.subset() %>% 
+            select(!selected.state) %>% 
+            rename(State = state, Stratification = Stratification1,
+                   `Total Mortality per Million` = total)
+            
     })
     
     # reactive to subset data to right topic
@@ -98,9 +113,27 @@ server <- function(input, output) {
             geom_bar(stat = "identity") +
             xlab("State") +
             ylab("Total Mortality per Million") +
-            scale_fill_manual(values = c("darkgray", "red")) +
+            scale_fill_manual(values = c("darkgray", "darkred")) +
             guides(fill = "none")
     })
+    
+    # data table output
+    output$us.table <- DT::renderDataTable(
+        DT::datatable(data = data.topic.subset.pretty(),
+                      options = list(pageLength = 10),
+                      rownames = FALSE)
+    )
+    
+    # download button
+    output$download.button <- downloadHandler(
+        # from documentation
+        filename = function() {
+            paste("movies_", str_replace_all(Sys.time(), ":|\ ", "_"), ".csv", sep = "")
+        },
+        content = function(file) {
+            write.csv(data.topic.subset.pretty(), file)
+        }
+    )
 }
 
 # Run the application 

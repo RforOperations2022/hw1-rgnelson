592ece1b4155083e87017fae0725b82bf7153e63
Bobby Nelson <rnelsonxy@gmail.com>
2022-02-04 11:58:30 -0500

    Adds Summary Bar Plot
    Adds a summary bar plot to compare the total mortality within that topic across the different states.

--- app/app.R
+++ app/app.R
@@ -1,12 +1,17 @@
 # import packages
+library(tidyverse)
+library(forcats)
 library(shiny)
 library(ggplot2)
 library(usmap)
 
 # read the data
 data <- readRDS(file="cdi_data.rds")
-minYear <- min(as.numeric(levels(data$YearStart))[data$YearStart])
-maxYear <- max(as.numeric(levels(data$YearStart))[data$YearStart])
+#data <- complete(data, state, YearStart)
+min.year <- min(as.numeric(levels(data$YearStart))[data$YearStart])
+max.year <- max(as.numeric(levels(data$YearStart))[data$YearStart])
+topic.list <- unique(data$Topic)
+state.list <- levels(data$state)
 
 # Define UI for Chronic Disease Indicators
 ui <- fluidPage(
@@ -17,45 +22,84 @@ ui <- fluidPage(
     # sidebar to select the chronic disease category
     sidebarLayout(
         sidebarPanel(
+            # select chronic disease
             selectInput(inputId = "cdi",
                         label = "Chronic Disease Category",
-                        choices = data$Topic,
-                        selected = "Alcohol")
+                        choices = topic.list,
+                        selected = topic.list[1]),
+            
+            # select state
+            selectInput(inputId = "state",
+                        label = "State",
+                        choices = c("All", state.list),
+                        selected = "All")
         ),
 
         mainPanel(
             # slider to select the year
             sliderInput(inputId = "year",
                         label = "Year",
-                        min = minYear,
-                        max = maxYear,
-                        value = maxYear,
+                        min = min.year,
+                        max = max.year,
+                        value = max.year,
                         sep = ""),
-            plotOutput("us.map")
+            
+            plotOutput("us.map"),
+            
+            plotOutput("us.barplot")
         )
     )
 )
 
 # Define server logic
 server <- function(input, output) {
-    data.subset <- reactive({
+    
+    # reactive to subset data to right topic
+    data.topic.subset <- reactive({
         req(input$cdi)
+        req(input$state)
         data %>% 
+            filter(Topic == input$cdi,
+                   YearStart == paste0(input$year)) %>%
+            group_by(Topic, state, Stratification1) %>%
+            summarize(total = sum(DataValue, na.rm=TRUE)) %>%
+            mutate(selected.state = ifelse(state == input$state,
+                                           TRUE, FALSE))
+    })
+    
+    # reactive to subset data to right topic
+    data.state.subset <- reactive({
+        req(input$state, input$cdi)
+        data %>% 
+            filter(if (input$state != "All") (state == input$state)) %>%
             filter(Topic == input$cdi,
                    YearStart == paste0(input$year)) %>%
             group_by(Topic, state, Stratification1) %>%
             summarize(total = sum(DataValue, na.rm=TRUE))
     })
     
+    
     # draw map of the US with heatmap by CDI (with overall stratification)
     output$us.map <- renderPlot({
-        plot_usmap(data = filter(data.subset(), Stratification1 == "Overall"), 
+        plot_usmap(data = filter(data.topic.subset(), Stratification1 == "Overall"), 
                    values = "total") + 
                    scale_fill_continuous(name = "Total Mortality per million",
                                          low = "white",
-                                         high = "red",
+                                         high = "darkred",
+                                         na.value = "blue",
                                          label=scales::comma) +
             theme(legend.position = "right")
+    })
+    
+    # draw bar plot of mortality in the topic by state
+    output$us.barplot <- renderPlot({
+        ggplot(data = data.topic.subset(),
+               aes(x = reorder(state, -total), y = total, fill = selected.state)) +
+            geom_bar(stat = "identity") +
+            xlab("State") +
+            ylab("Total Mortality per Million") +
+            scale_fill_manual(values = c("darkgray", "red")) +
+            guides(fill = "none")
     })
 }
 
